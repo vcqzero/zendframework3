a52c372e0adbd726a3b916ae5bed4046ad032841
vcqzero <1250857765@qq.com>
2018-11-17 09:32:47 +0800

    合并冲突

--- module/Api/src/Controller/TestController.php
+++ module/Api/src/Controller/TestController.php
@@ -28,50 +28,12 @@ class TestController extends AbstractActionController
     //index
     public function indexAction()
     {
-<<<<<<< HEAD
-        $manager = $this->SessionManager;
-//         $manager->forgetMe();
-//         $id = $manager->getId();
-//         $manager->destroy();
-        $manager->regenerateId();
-        $manager->start();
-//         $manager->setId('ppkpkpkk');
-        
-        
-//         $manager->setName('pp');
-=======
-        $Manager = $this->SessionManager;
-        $Manager->start();
-        $contianer = new Container('Zend_Auth', $Manager);
-        $contianer->storage = 'admin';
->>>>>>> origin/master
-        // DEBUG INFORMATION START
-        echo '------debug start------<br/>';
-        echo "<pre>";
-        var_dump(__METHOD__ . ' on line: ' . __LINE__);
-<<<<<<< HEAD
-//         var_dump($id);
-        var_dump($this->mySession->poo);
-        echo "</pre>";
-        exit('------debug end------');
-        // DEBUG INFORMATION END
-        
-        
-        
-        
-        
-        
-        
-        
         
         // DEBUG INFORMATION START
         echo '------debug start------<br/>';
         echo "<pre>";
         var_dump(__METHOD__ . ' on line: ' . __LINE__);
         var_dump();
-=======
-        var_dump($contianer->storage);
->>>>>>> origin/master
         echo "</pre>";
         exit('------debug end------');
         // DEBUG INFORMATION END
