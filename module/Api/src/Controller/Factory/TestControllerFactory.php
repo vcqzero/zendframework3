fa0b49fe21d05acff2ba1aec67a41d6b04308a46
vcqzero <1250857765@qq.com>
2018-11-17 09:28:15 +0800

    merge

--- module/Api/src/Controller/Factory/TestControllerFactory.php
+++ module/Api/src/Controller/Factory/TestControllerFactory.php
@@ -18,18 +18,10 @@ class TestControllerFactory implements FactoryInterface
         $UserCache = $container->get('UserCache');
         $filesystem= $container->get('filesystem');
         
-        // DEBUG INFORMATION START
-        echo '------debug start------<br/>';
-        echo "<pre>";
-        var_dump(__METHOD__ . ' on line: ' . __LINE__);
-        var_dump($UserCache);
-        echo "</pre>";
-        exit('------debug end------');
-        // DEBUG INFORMATION END
-        
         return new TestController(
             $container->get(UserManager::class),
-            $container->get(SessionManager::class)
+            $container->get(SessionManager::class),
+            $container->get('mySession')
             );
     }
 }
--- module/Api/src/Controller/TestController.php
+++ module/Api/src/Controller/TestController.php
@@ -11,23 +11,38 @@ class TestController extends AbstractActionController
 {
     private $UserManager;
     private $SessionManager;
+    private $mySession;
     public function __construct(
         UserManager $UserManager,
-        SessionManager $SessionManager
+        SessionManager $SessionManager,
+        $mySession
         )
     {
         $this->UserManager      = $UserManager;
         $this->SessionManager   = $SessionManager;
+        $this->mySession= $mySession;
+        $this->mySession->poo = 'qinchong';
     }
     
     //index
     public function indexAction()
     {
+        $manager = $this->SessionManager;
+//         $manager->forgetMe();
+//         $id = $manager->getId();
+//         $manager->destroy();
+        $manager->regenerateId();
+        $manager->start();
+//         $manager->setId('ppkpkpkk');
+        
+        
+//         $manager->setName('pp');
         // DEBUG INFORMATION START
         echo '------debug start------<br/>';
         echo "<pre>";
         var_dump(__METHOD__ . ' on line: ' . __LINE__);
-        var_dump('ok');
+//         var_dump($id);
+        var_dump($this->mySession->poo);
         echo "</pre>";
         exit('------debug end------');
         // DEBUG INFORMATION END
