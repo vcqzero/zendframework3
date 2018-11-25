d46ab88d8da9d9cfdf421e7a36c24b51b85eb536
vcqzero <1250857765@qq.com>
2018-11-25 22:53:44 +0800

    emial

--- module/Api/config/website.config/email.website.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'name' => 'smtp.exmail.qq.com',
-    'host' => 'pp',
-    'port' => '25',
-    'username' => 'test@tanhansi.com',
-    'password' => 'QINCHONG_admin_888_1',
-    'test_email' => '1250857765@qq.com',
-);
